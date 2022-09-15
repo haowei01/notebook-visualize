fe7687e0f0c3d57735fcb59bd8144f02dfd98a74
Haowei Zhang <haowei.zhang@airbnb.com>
2022-09-14 23:05:32 -0700

    add s2cell id from lat lng

--- geo/README.md
+++ geo/README.md
@@ -1,5 +1,5 @@
 ```commandline
 conda install folium -c conda-forge
-conda install -c conda-forge geojson_
+conda install -c conda-forge geojson
 pip install s2sphere
 ```
--- geo/plot_s2_cell.py
+++ geo/plot_s2_cell.py
@@ -2,7 +2,7 @@ import folium
 import s2sphere as s2
 
 
-def s2cell_to_polygon(s2cell):
+def s2cell_to_polygon(s2cell) -> list:
     poly = []
     for i in list(range(4)) + [0]:
         v = s2.LatLng.from_point(s2cell.get_vertex(i))
@@ -10,13 +10,18 @@ def s2cell_to_polygon(s2cell):
     return poly
 
 
-def plot_s2cell(s2cell_id):
+def plot_s2cell(s2cell_id: int):
     s2cell = s2.Cell(s2.CellId(s2cell_id))
     center = s2.LatLng.from_point(s2cell.get_center())
     m = folium.Map(location=[center.lat().degrees, center.lng().degrees])
     folium.PolyLine(s2cell_to_polygon(s2cell)).add_to(m)
     return m
 
+
+def s2cell_id_from_lat_lng(lat: float, lng: float, level: int) -> int:
+    """Get S2cell id from lat degree, lng degree, and level."""
+    s2_cell_id = s2.Cell.from_lat_lng(s2.LatLng.from_degrees(lat, lng)).id()
+    return s2_cell_id.parent(level).id()
 
 m = plot_s2cell(9290064014149156864)
 m.save('plot_s2_cell.html')
